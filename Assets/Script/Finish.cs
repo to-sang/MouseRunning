4e8a58c33883b89fc992568f4272ebcf3e5b992b
to-sang <tosang0197@gmail.com>
2022-12-17 07:10:14 +0700

    Finish Level System

--- Assets/Script/Finish.cs
+++ Assets/Script/Finish.cs
@@ -17,12 +17,22 @@ public class Finish : MonoBehaviour
         if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishRound.Play();
+            int curLv = int.Parse(System.IO.File.ReadAllLines("data.txt")[0]);
+            int lv = int.Parse(SceneManager.GetActiveScene().name.Substring(6));
+            if (lv == curLv)
+            {
+                curLv++;
+                System.IO.File.WriteAllText("data.txt", curLv.ToString());
+            }
             levelCompleted = true;
             Invoke("CompleteLevel", 2f);
         }
     }
     private void CompleteLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            SceneManager.LoadScene("Level");
     }
 }
--- Assets/Script/StartMenu.cs
+++ Assets/Script/StartMenu.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
+        if (EventSystem.current.currentSelectedGameObject.name.Equals("New Game"))
+            System.IO.File.WriteAllText("data.txt", "1");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
